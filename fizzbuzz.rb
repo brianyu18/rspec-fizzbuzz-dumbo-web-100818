be17fea3df4a69efb5f837894d8852f8ad263ed0
brianyu18 <brianyu1990@gmail.com>
2018-09-13 01:10:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (arg)
-  if arg % 3 == 0
+  if arg % 3 == 0 && arg % 5 == 0
+    return "FizzBuzz"
+  elsif arg % 3 == 0
     return "Fizz"
   elsif arg % 5 == 0
     return "Buzz"
-  else arg % 3 == 0 && arg % 5 == 0
-    return "FizzBuzz"
   end
 end
